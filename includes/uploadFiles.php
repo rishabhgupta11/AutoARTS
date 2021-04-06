bac7e0014c6f23de41bddb4fa2c1495c4bce9738
Rishabh Gupta <rishabh1998gupta@gmail.com>
2021-04-06 09:55:25 +0530

    fixed

--- includes/uploadFiles.php
+++ includes/uploadFiles.php
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 <?php
+
 require("../includes/connect.php");
 include("../includes/aws_s3_config.php");
 require("../vendor/autoload.php");
@@ -33,40 +33,5 @@ if(isset($_POST['uploadFiles']) && isset($_FILES['file']))
 		var_dump($result);
 	}
 }
-=======
-<?php
-require("../includes/connect.php");
-include("../includes/aws_s3_config.php");
-require("../vendor/autoload.php");
 
-$email = $_SESSION['email'];
-
-if(isset($_POST['uploadFiles']) && isset($_FILES['file']))
-{
-	$total = count($_FILES['file']['name']);
-
-	for( $i=0 ; $i < $total ; $i++ )
-	{
-		$file_name = $_FILES['file']['name'][$i];   
-		$temp_file_location = $_FILES['file']['tmp_name'][$i]; 
-
-		$s3 = new Aws\S3\S3Client([
-			'region'  => $region,
-			'version' => 'latest',
-			'credentials' => [
-				'key'    => $key,
-				'secret' => $secret,
-			]
-		]);		
-
-		$result = $s3->putObject([
-			'Bucket' => $bucket,
-			'Key'    => $email."/".$file_name,
-			'SourceFile' => $temp_file_location			
-		]);
-
-		var_dump($result);
-	}
-}
->>>>>>> cac54e50f10ab84f570b35eb65737a72e6f4cf08
 ?>
